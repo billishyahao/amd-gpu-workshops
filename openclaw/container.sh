21a39b8c10bf1b323d32a192412432fcd0d57b59
root <root@mia1-p99-g07.mia.tensorwave.lan>
2026-04-20 05:31:26 +0000

    update
    Signed-off-by: root <root@mia1-p99-g07.mia.tensorwave.lan>

---
+++ openclaw/container.sh
@@ -0,0 +1,1 @@
+docker run -it --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 32G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v /home/seungrok@amd.com/models:/models -v $(pwd):/workspace -w /workspace --name openclaw_srok --entrypoint /bin/bash  vllm/vllm-openai-rocm:v0.19.0
